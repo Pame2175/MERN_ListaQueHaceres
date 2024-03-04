7a9c0edec07e33d8715edf6b35358a30e37bfe41
Pame2175 <pamegonza.98.pg@gmail.com>
2024-03-04 16:08:18 -0300

    lista_tareas

---
+++ lista_de_queHaceres/src/App.jsx
@@ -0,0 +1,16 @@
+import React  from 'react';
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import TodoList from './componentes/lista_tareas'
+
+function App() {
+ 
+
+  return (
+    <>
+     < TodoList />
+    </>
+  )
+}
+
+export default App
